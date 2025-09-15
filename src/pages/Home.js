a628be52405cb88ce052eb1d37c59878cc368768
Vedantaz <muleyvedant4@gmail.com>
2025-09-15 20:53:54 +0530

    Commiting the changes did on UI using css, scss, and search logic

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -3,14 +3,13 @@ import ImproveSkills from "../components/ImproveSkills";
 import QouteSection from "../components/QuoteSection";
 import ChiefsSection from "../components/ChiefsSection";
 
-
-export default function Home(){
-    return (
-        <div>
-            <HeroSection />
-            <ImproveSkills />
-            <QouteSection />
-            <ChiefsSection />
-        </div>
-    )
+export default function Home() {
+  return (
+    <div>
+      <HeroSection />
+      <ImproveSkills />
+      <QouteSection />
+      <ChiefsSection />
+    </div>
+  );
 }
--- src/pages/Recipes.js
+++ src/pages/Recipes.js
@@ -1,79 +1,87 @@
-import PreviousSearches from "../components/PreviousSearches"
-import RecipeCard from "../components/RecipeCard"
+import PreviousSearches from "../components/PreviousSearches";
+import RecipeCard from "../components/RecipeCard";
 
-export default function Recipes(){
-    const recipes = [
-        {
-            title: "Chicken Pan Pizza",
-            image: "/img/gallery/img_1.jpg",
-            authorImg: "/img/top-chiefs/img_1.jpg",
-        }, 
-        {
-            title: "Spaghetti and Meatballs",
-            image: "/img/gallery/img_4.jpg",
-            authorImg: "/img/top-chiefs/img_2.jpg",
-        },
-        {
-            title: "American Cheese Burger",
-            image: "/img/gallery/img_5.jpg",
-            authorImg: "/img/top-chiefs/img_3.jpg",
-        },
-        {
-            title: "Mutton Biriyani",
-            image: "/img/gallery/img_6.jpg",
-            authorImg: "/img/top-chiefs/img_5.jpg",
-        },
-        {
-            title: "Japanese Sushi",
-            image: "/img/gallery/img_10.jpg",
-            authorImg: "/img/top-chiefs/img_6.jpg",
-        },
-        {
-            title: "Chicken Pan Pizza",
-            image: "/img/gallery/img_1.jpg",
-            authorImg: "/img/top-chiefs/img_1.jpg",
-        }, 
-        {
-            title: "Spaghetti and Meatballs",
-            image: "/img/gallery/img_4.jpg",
-            authorImg: "/img/top-chiefs/img_2.jpg",
-        },
-        {
-            title: "American Cheese Burger",
-            image: "/img/gallery/img_5.jpg",
-            authorImg: "/img/top-chiefs/img_3.jpg",
-        },
-        {
-            title: "Mutton Biriyani",
-            image: "/img/gallery/img_6.jpg",
-            authorImg: "/img/top-chiefs/img_5.jpg",
-        },
-        {
-            title: "Japanese Sushi",
-            image: "/img/gallery/img_10.jpg",
-            authorImg: "/img/top-chiefs/img_6.jpg",
-        },
-        {
-            title: "American Cheese Burger",
-            image: "/img/gallery/img_5.jpg",
-            authorImg: "/img/top-chiefs/img_3.jpg",
-        },
-        {
-            title: "Mutton Biriyani",
-            image: "/img/gallery/img_6.jpg",
-            authorImg: "/img/top-chiefs/img_5.jpg",
-        }
-    ].sort(() => Math.random() - 0.5)
+export default function Recipes({ query, setQuery }) {
+  const recipes = [
+    {
+      title: "Chicken Pan Pizza",
+      image: "/img/gallery/img_1.jpg",
+      authorImg: "/img/top-chiefs/img_1.jpg",
+    },
+    {
+      title: "Spaghetti and Meatballs",
+      image: "/img/gallery/img_4.jpg",
+      authorImg: "/img/top-chiefs/img_2.jpg",
+    },
+    {
+      title: "American Cheese Burger",
+      image: "/img/gallery/img_5.jpg",
+      authorImg: "/img/top-chiefs/img_3.jpg",
+    },
+    {
+      title: "Mutton Biriyani",
+      image: "/img/gallery/img_6.jpg",
+      authorImg: "/img/top-chiefs/img_5.jpg",
+    },
+    {
+      title: "Japanese Sushi",
+      image: "/img/gallery/img_10.jpg",
+      authorImg: "/img/top-chiefs/img_6.jpg",
+    },
+    {
+      title: "Chicken Pan Pizza",
+      image: "/img/gallery/img_1.jpg",
+      authorImg: "/img/top-chiefs/img_1.jpg",
+    },
+    {
+      title: "Spaghetti and Meatballs",
+      image: "/img/gallery/img_4.jpg",
+      authorImg: "/img/top-chiefs/img_2.jpg",
+    },
+    {
+      title: "American Cheese Burger",
+      image: "/img/gallery/img_5.jpg",
+      authorImg: "/img/top-chiefs/img_3.jpg",
+    },
+    {
+      title: "Mutton Biriyani",
+      image: "/img/gallery/img_6.jpg",
+      authorImg: "/img/top-chiefs/img_5.jpg",
+    },
+    {
+      title: "Japanese Sushi",
+      image: "/img/gallery/img_10.jpg",
+      authorImg: "/img/top-chiefs/img_6.jpg",
+    },
+    {
+      title: "American Cheese Burger",
+      image: "/img/gallery/img_5.jpg",
+      authorImg: "/img/top-chiefs/img_3.jpg",
+    },
+    {
+      title: "Mutton Biriyani",
+      image: "/img/gallery/img_6.jpg",
+      authorImg: "/img/top-chiefs/img_5.jpg",
+    },
+  ].sort(() => Math.random() - 0.5);
 
+  const filteredRecipes = recipes.filter((receipe) => {
+    const q = query.toLowerCase();
     return (
-        <div>
-            <PreviousSearches />
-            <div className="recipes-container">
-                {/* <RecipeCard /> */}
-                {recipes.map((recipe, index) => (
-                    <RecipeCard key={index} recipe={recipe} />
-                ))}
-            </div>
-        </div>
-    )
+      receipe.title.toLowerCase().includes(q) ||
+      receipe.authorImg.toLowerCase().includes(q) ||
+      receipe.image.toLowerCase().includes(q)
+    );
+  });
+
+  return (
+    <div>
+      <PreviousSearches query={query} setQuery={setQuery} />
+      <div className="recipes-container">
+        {filteredRecipes.map((recipe, index) => (
+          <RecipeCard key={index} recipe={recipe} />
+        ))}
+      </div>
+    </div>
+  );
 }
